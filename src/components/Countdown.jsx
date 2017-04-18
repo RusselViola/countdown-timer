fb7d25c470a3418a9e89b8802ec4d2a2387758d9
RUSSEL VIOLA <RUSSELVIOLA@GMAIL.COM>
2017-04-18 18:11:00 -0400

    form into Countdown WIP

--- src/components/Countdown.jsx
+++ src/components/Countdown.jsx
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
 import Clock from 'Clock';
+import CountdownFrom from 'CountdownForm';
 
 export default class Countdown extends Component {
+  
+
+  handleSetCountDown() {
+
+  }
 
   render() {
     return (
       <div>
         <Clock totalSeconds={129}/>
+        <CountdownForm
+          onSetCountdown={this.handleSetCountdown}
+        />
       </div>
     )
   }
