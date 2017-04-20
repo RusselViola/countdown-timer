1ec548e07bedf1fc6df81b2016fa9ddd1f820b1c
RUSSEL VIOLA <RUSSELVIOLA@GMAIL.COM>
2017-04-20 18:06:14 -0400

    spy test for valid numeral in CountdownForm

--- src/tests/components/CountdownForm.test.jsx
+++ src/tests/components/CountdownForm.test.jsx
@@ -9,5 +9,20 @@ import CountdownForm from 'CountdownForm';
 describe('CountdownForm', () => {
   it('should exist', () => {
     expect(CountdownForm).toExist();
-  })
-})
+  });
+
+  it('should call onSetCountdown if valid seconds entered', () => {
+    let spy = expect.createSpy();
+    let countdownForm = TestUtils.renderIntoDocument(
+      <CountdownForm
+        onSetCountdown={spy}
+      />
+    );
+    let $el = $(ReactDOM.findDOMNode(countdownForm));
+
+    countdownForm.refs.seconds.value = '109';
+    TestUtils.Simulate.submit($el.find('form')[0]);
+
+    expect(spy).toHaveBeenCalledWith(109);
+  });
+});
