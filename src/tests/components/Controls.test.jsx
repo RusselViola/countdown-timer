14f0d51c1006ec3260f17ad7bacddcbc66caef5e
RUSSEL VIOLA <RUSSELVIOLA@GMAIL.COM>
2017-04-27 17:48:02 -0400

    first controls test

---
+++ src/tests/components/Controls.test.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Controls from 'Controls';
+
+const expect = require('expect');
+const $ = require('jQuery');
+const TestUtils = require('react-addons-test-utils');
+
+describe('Controls', () => {
+  it('should exist', () => {
+    expect(Controls).toExist;
+  })
+})
