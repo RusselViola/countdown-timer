6f8b33c6cd76192d60532b2699a8477bc82fa453
RUSSEL VIOLA <RUSSELVIOLA@GMAIL.COM>
2017-05-08 17:32:11 -0400

    'stopped' state test working

--- src/tests/components/Timer.test.jsx
+++ src/tests/components/Timer.test.jsx
@@ -39,17 +39,17 @@ describe('Timer', () => {
     }, 1001)
   });
 
-  // it('should pause timer on paused status', () => {
-  //   let timer = TestUtils.renderIntoDocument(<Timer/>);
-  //
-  //   timer.setState({count: 10});
-  //   timer.handleStatusChange('started');
-  //   timer.handleStatusChange('paused');
-  //
-  //   setTimeout(() => {
-  //     expect(timer.state.timerStatus).toBe('paused');
-  //     expect(timer.state.count).toBe(10);
-  //     done();
-  //   }, 1001)
-  // });
+  it('should clear count on stopped status', () => {
+    let timer = TestUtils.renderIntoDocument(<Timer/>);
+
+    timer.setState({count: 10});
+    timer.handleStatusChange('started');
+    timer.handleStatusChange('stopped');
+
+    setTimeout(() => {
+      expect(timer.state.timerStatus).toBe('stopped');
+      expect(timer.state.count).toBe(0);
+      done();
+    }, 1001)
+  });
 });
