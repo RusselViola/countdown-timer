08cd9960eaade4c38370599636e47d8d733e7138
RUSSEL VIOLA <RUSSELVIOLA@GMAIL.COM>
2017-05-08 17:16:28 -0400

    should exist

---
+++ src/tests/components/Timer.test.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Timer from 'Timer';
+
+const expect = require('expect');
+const $ = require('jQuery');
+const TestUtils = require('react-addons-test-utils');
+
+describe('Timer', () => {
+  it('should exist', () => {
+    expect(Timer).toExist();
+
+  });
+});
