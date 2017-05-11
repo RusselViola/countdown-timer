8ae8c537bec4f07294cd6257c89f65c8021b62f4
RUSSEL VIOLA <RUSSELVIOLA@GMAIL.COM>
2017-05-11 17:27:36 -0400

    foundation scss loader

--- src/app.jsx
+++ src/app.jsx
@@ -6,7 +6,6 @@ import Timer from 'Timer';
 import Countdown from 'Countdown';
 
 //load foundation
-require('style!css!foundation-sites/dist/foundation.min.css');
 $(document).foundation();
 
 //App css
